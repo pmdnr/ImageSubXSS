5e884048a9076a4b872da96e664267d16e816151
PMD Nagarjun <pmdnr@nagabot.com>
2017-07-25 14:40:53 +0530

    Added Information about Developers and How to use package

--- ImageSubXSS.php
+++ ImageSubXSS.php
@@ -1,4 +1,17 @@
 <?php
-//comment
+/* 
+--by PMD Nagarjun and Dr. Shaik Shakeel Ahamad-------------------------
+Solution to prevent Cross Site Scripting Attacks in Web Applications
+-----------------------------------------------------------------------
+How to use : include this file in the beginning of your .php files
+which process user data.
+-----------------------------------------------------------------------
+ImageSubXSS is licensed under the MIT License
+-----------------------------------------------------------------------
+Check pmdnr.com for additional information
+-----------------------------------------------------------------------
+*/
+
+
 
 ?>
